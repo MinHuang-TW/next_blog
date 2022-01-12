e021d74e7566b779cd5b4d814d624f0ad223c2fd
Min Huang <h.min719@gmail.com>
2022-01-12 15:04:54 +0800

    Added img-src for CSP

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const securityHeaders = [
   {
     key: 'Content-Security-Policy',
     value:
-      "default-src 'self' https://api-eu-central-1.graphcms.com/ https://media.graphcms.com/ https://vitals.vercel-insights.com/; font-src: https://fonts.googleapis.com/",
+      "default-src 'self' https://api-eu-central-1.graphcms.com/ https://media.graphcms.com/ https://vitals.vercel-insights.com/ https://fonts.googleapis.com/; img-src 'self' data:image/svg+xml,image/gif",
   },
 ];
 
