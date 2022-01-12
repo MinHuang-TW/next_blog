35e3f1228bafe98db8b0f9b92bac47a27fad7a33
Min Huang <h.min719@gmail.com>
2022-01-12 15:10:15 +0800

    Added font-src for CSP

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const securityHeaders = [
   {
     key: 'Content-Security-Policy',
     value:
-      "default-src 'self' https://api-eu-central-1.graphcms.com/ https://media.graphcms.com/ https://vitals.vercel-insights.com/ https://fonts.googleapis.com/; img-src 'self' data:image/svg+xml,image/gif",
+      "default-src 'self' https://api-eu-central-1.graphcms.com/ https://vitals.vercel-insights.com/ https://fonts.googleapis.com/;font-src https://fonts.gstatic.com/;img-src 'self' https://media.graphcms.com/ data:image/svg+xml,image/gif",
   },
 ];
 
